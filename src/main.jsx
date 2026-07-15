d786577996132d3a55a90c1b47facedf9ea9fd0f
gopibhatnagar16 <gopibhatnagar@gmail.com>
2026-07-15 15:05:55 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import AboutTemplate from './App.jsx'
+import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AboutTemplate />
+    <App />
   </React.StrictMode>
 )
